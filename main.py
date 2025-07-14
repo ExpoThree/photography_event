7291cc145990bbfb0a8da846c6ba275b2ff99441
ExpoThree <expothree@users.noreply.github.com>
2025-07-14 13:08:57 +0530

    feat(submissions): add view messages db, removesumbmission commmand

--- main.py
+++ main.py
@@ -37,8 +37,11 @@ conn.close()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 SUBMISSIONS_CHANNEL = 1393120849397157899
+DISCUSSIONS_CHANNEL = 1375524101858398268
 MODERATOR_ROLE = 1371013866212950127
 GUILD_ID = 1371013559571583050
+
+bot_deleted_messages = set()
 
 intents = discord.Intents.default()
 intents.message_content = True 
@@ -57,6 +60,95 @@ async def deletedb(interaction: discord.Interaction):
     conn.commit()
     conn.close()
     await interaction.response.send_message("🗑️ All entries deleted.", ephemeral=True)
+
+@client.tree.command(name="viewdb",
+                     description="View the entries present in the database",
+                     guild=discord.Object(id=GUILD_ID))
+@app_commands.checks.has_role(MODERATOR_ROLE)
+async def viewdb(interaction: discord.Interaction):
+
+    # prevent timeout
+    await interaction.response.defer(ephemeral=True)
+
+    conn = sqlite3.connect('messages.db')
+    c = conn.cursor()
+    c.execute('SELECT username, userid, messageid, threadid FROM messages')
+    rows = c.fetchall()
+    conn.close()
+
+    if not rows:
+        await interaction.followup.send(":x: No entries found in the database.", ephemeral=True)
+        return
+
+    content = "username, userid, messageid, threadid\n"
+    content += "\n".join([f"{row[0]}, {row[1]}, {row[2]}, {row[3]}" for row in rows])
+
+    temp_file = "db_dump.txt"
+
+    with open(temp_file, "w") as f:
+        f.write(content)
+
+    await interaction.followup.send("📄 Database entries: ", file=discord.File(temp_file), ephemeral=True)
+
+    # delete the temp file
+    os.remove(temp_file)
+
+@client.tree.command(name="removesubmission",
+                     description="Delete the submission of an user",
+                     guild=discord.Object(id=GUILD_ID))
+@app_commands.checks.has_role(MODERATOR_ROLE)
+@app_commands.describe(user="User whose submission you want to remove",
+                       reason="Reason for deletion (sent to user via DM)")
+async def removesubmission(interaction: discord.Interaction, user: discord.Member, reason: str):
+
+    # prevent timeout
+    await interaction.response.defer(ephemeral=True)
+
+    conn = sqlite3.connect("messages.db")
+    c = conn.cursor()
+
+    c.execute("SELECT id, messageid, threadid FROM messages WHERE userid = ?", (user.id,))
+    result = c.fetchone()
+    conn.close()
+
+    if not result:
+        await interaction.followup.send(":x: No entries found for this user.", ephemeral=True)
+        return
+    
+    id, message_id, thread_id = result
+
+    try:
+        channel = interaction.guild.get_channel(SUBMISSIONS_CHANNEL)
+        message = await channel.fetch_message(message_id)
+        
+        bot_deleted_messages.add(message.id)
+        await message.delete()
+
+    except Exception as e:
+        await interaction.followup.send(f"⚠️ Failed to delete message:\n```{e}```", ephemeral=True)
+        return
+
+    try:
+        thread = await interaction.guild.fetch_channel(thread_id)
+        await thread.delete()
+    except Exception as e:
+        await interaction.followup.send(f"⚠️ Failed to delete thread:\n```{e}```", ephemeral=True)
+        return
+
+    await interaction.followup.send(f"Submission of {user.mention} with ID {id} deleted successfully.", ephemeral=True)
+
+    channel = interaction.guild.get_channel(SUBMISSIONS_CHANNEL)
+
+    try:
+        await user.send(f"Your submission for the **SW Meme Event** in <#{SUBMISSIONS_CHANNEL}> is removed by the moderators because of the reason: {reason}.")
+    except Exception:
+        await channel.send(f"{user.mention} Your submission for the **SW Meme Event** is removed by the moderators because of the reason: {reason}.")
+
+    conn = sqlite3.connect("messages.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM messages WHERE userid = ?", (user.id,))
+    conn.commit()
+    conn.close()
 
 @client.command()
 async def ping(ctx):
@@ -111,22 +203,30 @@ async def on_message(message):
             isMedia = True
 
     if not isMedia:
+
+        bot_deleted_messages.add(message.id)
         await message.delete()
-        
+
         await message.channel.send("You are not allowed to send anything other than images here.", delete_after = 10)
         
         await client.process_commands(message)
         return
     
     if len(message.attachments) > 1:
+
+        bot_deleted_messages.add(message.id)
         await message.delete()
+
         await message.channel.send("You are not allowed to send more than one submission.", delete_after = 10)
 
         await client.process_commands(message)
         return
 
     if user_exists(message.author.id):
+
+        bot_deleted_messages.add(message.id)
         await message.delete()
+
         await message.channel.send("You have already posted your submission. If you want to change / edit your submission, delete your original submission and create a new one.", delete_after = 10)
         
         await client.process_commands(message)
@@ -157,6 +257,10 @@ async def on_message(message):
 @client.event
 async def on_message_delete(message):
 
+    if message.id in bot_deleted_messages:
+        bot_deleted_messages.remove(message.id)
+        return
+        
     isMedia = False
     
     if client.user == message.author:
